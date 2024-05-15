80aaec3ee4b855b03f0e50fa95ea3eb8255cc7f1
Ivan Popovych <ipopovych2004@gmail.com>
2024-05-15 21:37:06 +0300

    add aliases

--- src/pages/selectProject/SelectProject.tsx
+++ src/pages/selectProject/SelectProject.tsx
@@ -1,8 +1,9 @@
-import { userService } from '../../api/services/user/user.service';
-import { ProjectList } from '../../components/ProjectList/ProjectList';
-import { UserInfo } from '../../components/UserInfo/UserInfo';
-import { useLoading } from '../../hooks/useLoading';
-import { useAppSelector } from '../../redux/hooks';
+import { userService } from '@/api/services/user/user.service';
+import { ProjectList } from '@/components/ProjectList/ProjectList';
+import { UserInfo } from '@/components/UserInfo/UserInfo';
+import { useLoading } from '@/hooks/useLoading';
+import { useAppSelector } from '@/redux/hooks';
+
 import { SelectProjectHeader } from './libs/components/SelectProjectHeader';
 
 export const SelectProject = () => {
